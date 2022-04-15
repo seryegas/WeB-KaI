cb709c82d6fc1502b15b8d6fcfc3fc8d449a77fb
seryegas <sergeu.shakirov@gmail.com>
2022-04-15 09:31:06 +0300

    interactive work and update

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
-var main = function () {
-    "use strict";
-    var tabNumber;
-    for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
-        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-         $(tabSelector).on("click", function () {
-             $(".tabs span").removeClass("active");
-             $(this).addClass("active");
-             return false;
-        });
-    }
-};
+var toDos = [
+    "Закончить писать эту книгу",
+    "Вывести Грейси на прогулку в парк",
+    "Ответить на электронные письма",
+    "Подготовиться к лекции в понедельник",
+    "Обновить несколько новых задач",
+    "Купить продукты"
+    ];
 
-$(document).ready(main);
+    $("document").ready( function(){
+    $(".tabs a span").toArray().forEach(function (element) {
+        $(element).on("click", function () {
+            var $element = $(element);
+            $(".tabs a span").removeClass("active");
+            $(element).addClass("active");
+            $("main .content").empty();
+
+            if ($element.parent().is(":nth-child(1)")) {
+                for (var i = toDos.length-1; i > -1; i--) {
+                    $(".content").append($("<li>").text(toDos[i]));
+                }
+            }
+            else if ($element.parent().is(":nth-child(2)")) {
+                toDos.forEach(function (todo) {
+                    $(".content").append($("<li>").text(todo));
+                });
+            }
+            else if ($element.parent().is(":nth-child(3)")) {
+                $(".content").append(
+                    '<input type="text" class="inp">'+
+                    '<button class="btn">Добавить</button>'
+                );
+                var newToDo;
+                $('.btn').on('click',function(){
+                    newToDo= $('.inp').val();
+                    if (newToDo!='') {
+                        toDos.push( newToDo);
+                        alert('Новое задание "'+newToDo+'" успешно добавлено!');
+                        $('.inp').val("");
+                    }
+                })
+
+            }
+            return false;
+        })
+    })
+    $(".tabs a:first-child span").trigger("click");
+}) 
