bd0f1f7f7de9ec4ceff43c6c90cd110deebcdd7a
seryegas <sergeu.shakirov@gmail.com>
2022-04-28 16:42:24 +0300

    finished tags

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+var organizeByTags = function (toDoObjects) {
+
+	console.log("organizeByTags called");
+	// создание пустого массива для тегов
+	var tags = [];
+	// перебираем все задачи toDos
+	toDoObjects.forEach(function (toDo) {
+		// перебираем все теги для каждой задачи
+		toDo.tags.forEach(function (tag) {
+			// убеждаемся, что этого тега еще нет в массиве
+			if (tags.indexOf(tag) === -1) {
+				tags.push(tag);
+			}
+		});
+	});
+	console.log(tags);
+	var tagObjects = tags.map(function (tag) {
+		// здесь мы находим все задачи,
+		// содержащие этот тег
+		var toDosWithTag = [];
+		toDoObjects.forEach(function (toDo) {
+			// проверка, что результат
+			// indexOf is *не* равен -1
+			if (toDo.tags.indexOf(tag) !== -1) {
+				toDosWithTag.push(toDo.description);
+			}
+		});
+		// мы связываем каждый тег с объектом, который // содержит название тега и массив
+		return { "name": tag, "toDos": toDosWithTag };
+	});
+	console.log(tagObjects);
+	return tagObjects;
+};
+
+
 var main = function (toDoObjects) {
   "use strict";
   var toDos = toDoObjects.map(function (toDo) {
@@ -26,34 +61,7 @@ var main = function (toDoObjects) {
             });
           } else if ($element.parent().is(":nth-child(3)")) {
             console.log("Щелчок на вкладке Теги");
-            var organizedByTag =
-					[
-					{
-					"name": "покупки",
-					"toDos": ["Купить продукты"]
-					},
-					{
-					"name": "рутина",
-					"toDos": ["Купить продукты", "Вывести Грейси на прогулку в парк"]
-					},
-					{
-					"name": "писательство",
-					"toDos": ["Сделать несколько новых задач", "Закончить писать книгу"]
-					},
-					{
-					"name": "работа",
-					"toDos": ["Сделать несколько новых задач", "Подготовиться к лекции в понедельник",
-					"Ответить на электронные письма", "Закончить писать книгу"]
-					},
-					{
-					"name": " преподавание",
-					"toDos": ["Подготовиться к лекции в понедельник"]
-					},
-					{
-					"name": "питомцы",
-					"toDos": ["Вывести Грейси на прогулку в парк "]
-					}
-					]
+            var organizedByTag = organizeByTags(toDoObjects);
 					organizedByTag.forEach(function (tag) {
 						var $tagName = $("<h3>").text(tag.name),
 						$content = $("<ul>");
@@ -78,6 +86,24 @@ var main = function (toDoObjects) {
                 $(".inp").val("");
               }
             });
+            var $input = $("<input>").addClass("description"),
+					$inputLabel = $("<h5>").text("Новая задача: "),
+					$tagInput = $("<input>").addClass("tags"),
+					$tagLabel = $("<h5>").text("Тэги: "),
+					$button = $("<button>").text("+");
+					$button.on("click", function () {
+						var description = $input.val(),
+						// разделение в соответствии с запятыми
+						tags = $tagInput.val().split(",");
+						toDoObjects.push({"description":description, "tags":tags});
+						// обновление toDos
+						toDos = toDoObjects.map(function (toDo) {
+							return toDo.description;
+						});
+						$input.val("");
+						$tagInput.val("");
+					});
+				$("main .content").append($inputLabel).append($input).append($tagLabel).append($tagInput).append($button);
           }
           return false;
         });
